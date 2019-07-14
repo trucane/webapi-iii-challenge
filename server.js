3dab8b5692bc6cd0b819436e23842eed3fdcbe37
trucane <trufashionista2013@gmail.com>
2019-07-14 16:39:46 -0500

    WIP finished MVP

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = 'express';
+const express = require('express');
+const router = require('./users/userRouter')
+
+
 
 const server = express();
+
+
+//custom middleware
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} ${req.get('Origin')}`);
+  next();
+};
+
+
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+server.use('/api/users', router);
 
-function logger(req, res, next) {
+const port = 5000;
 
-};
+server.listen(port, () => console.log('I am on port 5000'))
+
 
 module.exports = server;
